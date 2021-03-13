b5b725acaa23c6ec12ad6ebf0f8b8d57960950a2
Medina Pilavdzic <medina.pilavdzic@gmail.com>
2021-03-13 02:21:44 +0100

    Assignment for Day05

---
+++ ex08/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    // Only change code below this line
+    var lastNameLength = "Doe".length;
+
+    // Only change code above this line
+
+    return lastNameLength;
+
+}
+
+console.log(main());
+module.exports = main;
